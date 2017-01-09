a7b1644e9acdf7bf33fe3fd828b113d539f0faa9
Chris Chinchilla <chriswhward@gmail.com>
2017-01-09 17:00:33 +0100

    Use image resizing

--- site/javascripts/components/Page.js
+++ site/javascripts/components/Page.js
@@ -11,7 +11,7 @@ export default function render(props) {
   }
   return <div className="container page">
     <h1>&gt; {props.fields.title}</h1>
-    {props.fields.image ? <img style={{maxWidth: '100%'}} src={'https:' + props.fields.image.fields.file.url}/> : ''}
+    {props.fields.image ? <img style={{maxWidth: '100%'}} src={'https:' + props.fields.image.fields.file.url  + '?w=450'} /> : ''}
     <p>{props.fields.body}</p>
     </div>
 }
