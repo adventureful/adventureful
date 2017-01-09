1687c8c13a533be502d45a6fd182f222f04a4f0b
Sergiy Tupchiy <sergiy@contentful.com>
2017-01-09 19:57:53 +0100

    Tests: added some real test validations

--- tests/src/test/java/com/contentful/adventureful/AdventurefulTest.java
+++ tests/src/test/java/com/contentful/adventureful/AdventurefulTest.java
@@ -3,7 +3,9 @@ package com.contentful.adventureful;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
+import static org.testng.Assert.assertTrue;
 
 
 /**
@@ -17,6 +19,16 @@ public class AdventurefulTest {
 		open("/");
 		for (int i = 0; i < RandomChoice.choices; i++) {
 			sleep(timeout);
+
+			/** Validate presence of Image Asset */
+			$(".container.page img").shouldBe(visible);
+
+			/** Check images loaded with Images API*/
+			String src = $(".container.page img").getAttribute("src");
+			assertTrue(src.contains("images.contentful.com"), "Wrong url used in 'src' attr. Value:\n" + src);
+
+			/** Validate presence of  Choice Field*/
+			$(".container.page p").shouldBe(visible);
 			try {
 				RandomChoice.getAnyElement($$(By.cssSelector(".list-group-item a"))).click();
 			} catch (Exception e) {
