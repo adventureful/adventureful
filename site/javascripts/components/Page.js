4696c3a5334081e3aaa567f813bf5dee01323b0c
Maria Kuzovchikova <iseesomuch@gmail.com>
2017-01-09 12:11:16 +0100

    frontend stuuff

---
+++ site/javascripts/components/Page.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default function render(props) {
+  console.log(props);
+  return <div className="container page">
+    Hello! This is a page!
+    </div>
+}
--- site/javascripts/index.js
+++ site/javascripts/index.js
@@ -1,4 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Page from './components/Page';
 
-console.log('helloo')
+getPage().then(function(data) {
+  ReactDOM.render(
+    <div className="container">
+      <Page data={data}/>
+    </div>,
+    document.getElementById('root')
+  );
+})
+
+function getPage() {
+  return new Promise((resolve, reject) => resolve({ title: "hello" }));
+}
