c140234ca7c96101370ad4374eeabe1b4327fb48
Chris Chinchilla <chriswhward@gmail.com>
2017-01-09 15:39:21 +0100

    Use correct field for content type

--- site/javascripts/components/Page.js
+++ site/javascripts/components/Page.js
@@ -10,7 +10,7 @@ export default function render(props) {
     </div>
   }
   return <div className="container page">
-    <h1>&gt; {props.fields.name}</h1>
+    <h1>&gt; {props.fields.title}</h1>
     {props.fields.image ? <img style={{maxWidth: '100%'}} src={'https:' + props.fields.image.fields.file.url}/> : ''}
     <p>{props.fields.body}</p>
     </div>
